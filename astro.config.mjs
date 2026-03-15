8cf6b180165a61a20c4d7f2692c502467732c788
Mikan-Account <kanaya3561@gmail.com>
2026-03-15 09:16:44 +0900

    fix: remove legacy remark-base-url plugin to fix broken images

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,10 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-import remarkBaseUrl from './remark-base-url.mjs';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://min-note.com',
 	base: '/',
 	integrations: [mdx(), sitemap()],
-	markdown: {
-		remarkPlugins: [remarkBaseUrl],
-	},
 });
